6b70cd66e17453416139bdc158971bbf96ced723
AdamPtk <adam.pietkiewicz12@gmail.com>
2021-08-07 16:25:26 +0200

    Contact section validation

--- src/components/HomeContactForm.js
+++ src/components/HomeContactForm.js
@@ -1,10 +1,44 @@
+import {useState} from "react";
+
 import HeaderDecoration from "./atoms/HeaderDecoration";
 
 const HomeContactForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const validateStyle = {
+        color: "red"
+    }
+
+    const validateName = name => {
+        const re = /^[a-zA-Z]+$/
+        console.log(re.test(String(name).toLowerCase()))
+        return name.length !== 0 ? re.test(String(name).toLowerCase()) : false;
+    }
+
+    const validateEmail = email => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        console.log(re.test(String(email).toLowerCase()))
+        return email.length !== 0 ? re.test(String(email).toLowerCase()) : false;
+    }
+
+    const validateMessage = message => {
+        const length = message.length;
+        console.log(length > 120 ? true : false);
+        return length > 120 ? true : false;
+    }
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        validateName(name);
+        validateEmail(email);
+        validateMessage(message);
+    }
     return (
         <div className="form-section">
             <HeaderDecoration text1="Contact us!" />
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <div className="user-data">
                     <label>
                         Type your name
@@ -12,8 +46,12 @@ const HomeContactForm = () => {
                             id="name" 
                             name="name" 
                             type="text" 
-                            placeholder="John" 
+                            placeholder="John"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            style={name.length === 0 || validateName(name) ? null : {borderBottom: "1px solid red"}}
                         />
+                        {(name.length === 0 || validateName(name)) ? null : <span style={validateStyle}>The given name is incorrect</span>}
                     </label>
                     <label>
                         Type your email
@@ -22,9 +60,14 @@ const HomeContactForm = () => {
                             name="email" 
                             type="email" 
                             placeholder="xyz@abc.com"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            style={email.length === 0 || validateEmail(email) ? null : {borderBottom: "1px solid red"}}
                         />
+                        {(email.length === 0 || validateEmail(email)) ? null : <span style={validateStyle}>The given email is incorrect</span>}
                     </label>
                 </div>
+
                 <div className="user-message">
                     <label htmlFor="message">Type your message</label>
                     <textarea 
@@ -36,7 +79,11 @@ const HomeContactForm = () => {
                             elit, sed do eiusmod tempor incididunt ut labore et dolore 
                             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                             ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                        style={message.length === 0 || validateMessage(message) ? null : {borderBottom: "1px solid red"}}
                         />
+                        {(message.length === 0 || validateMessage(message)) ? null : <span style={validateStyle}>The message must be at least 120 characters long</span>}
                 </div>
                 <input id="submit" type="submit" value="Send" />
             </form>
