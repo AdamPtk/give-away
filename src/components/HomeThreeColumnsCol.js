6c698282359d84016c4646c0136d7f961baf4b10
AdamPtk <adam.pietkiewicz12@gmail.com>
2021-08-15 13:15:00 +0200

    Creating readme file

--- src/components/HomeThreeColumnsCol.js
+++ src/components/HomeThreeColumnsCol.js
@@ -4,15 +4,15 @@ import CountUp from 'react-countup';
 import VisibilitySensor from "react-visibility-sensor";
 
 const HomeThreeColumnsCol = ({count, item}) => {
-    const [state, setState] = useState({visible: false});
+    const [state1, setState1] = useState({visible: false});
 
     const onChangeVisibility = isActive => {
-        setState({ ...state, visible: isActive });
+        setState1({ ...state1, visible: isActive });
     }
 
     return (
         <div className='column'>
-            <VisibilitySensor onChange={e => onChangeVisibility(e)} active={!state.visible} partialVisibility offset={{ bottom: 200 }}>
+            <VisibilitySensor onChange={e => onChangeVisibility(e)} active={!state1.visible} partialVisibility offset={{ bottom: 200 }}>
                 {({ isVisible }) => (
                     <div>
                         {isVisible ? <CountUp start={0} end={count} duration={2}/> : <span>0</span>}
